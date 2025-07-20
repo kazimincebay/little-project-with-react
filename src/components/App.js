e8780180f88abf0a9f085058de4bce43dece544f
Kazım İncebay <kazimincebay@gmail.com>
2025-07-20 09:01:24 +0300

    I send parameters to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { Component } from "react";
 import SearchBar from "./SearchBar/SearchBar";
 import "./App.css";
-const App = () => {
-  return (
-    <div className="app-container">
-      <SearchBar />
-      State değer atama
-    </div>
-  );
-};
+class App extends Component{
+  onSearchImage = search =>{
+    console.log("App "+search);
+  }
+  render() {
+    return (
+      <div className="app-container">
+        <SearchBar onSearchImage={this.onSearchImage}/>
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,20 +2,38 @@ import React, { Component } from "react";
 import "./SearchBar.css";
 class SearchBar extends Component {
   state = {
-search:''
+    search: "",
   };
-  inputChanged(event){
-    console.log(event.target.value)
+  inputChanged(event) {
+    console.log(event.target.value);
     this.setState({
-      search:event.target.value
-    })
+      search: event.target.value,
+    });
   }
-  render(){
-  return (
-<div className="search-bar-container ui input">
-    <input type="text" onChange={this.inputChanged.bind(this)} placeholder="Search..."/>
-</div>    
-  )
-}};
+
+  searchImage = () => {
+    console.log("Success");
+    this.props.onSearchImage(this.state.search);
+  };
+  render() {
+    return (
+      <div className="search-bar-container ui action input">
+        <input
+          type="text"
+          onKeyPress={(e) => {
+            if (e.key === "Enter") {
+              this.searchImage();
+            }
+          }}
+          onChange={this.inputChanged.bind(this)}
+          placeholder="Search..."
+        />
+        <button className="ui icon-button" onClick={this.searchImage}>
+          <i className="search icon"></i>
+        </button>
+      </div>
+    );
+  }
+}
 
 export default SearchBar;
