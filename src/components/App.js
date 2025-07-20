2177bba49810045ebb7331f62d1a500a9cef0178
Kazım İncebay <kazimincebay@gmail.com>
2025-07-20 09:34:22 +0300

    I install axios and I send get request Unsplash api with axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,32 @@
 import React, { Component } from "react";
 import SearchBar from "./SearchBar/SearchBar";
 import "./App.css";
-class App extends Component{
-  onSearchImage = search =>{
-    console.log("App "+search);
+import axios from "axios";
+
+class App extends Component {
+  state={
+    images:[]
   }
+   onSearchImage = async (search) => {
+    console.log("App " + search);
+   const result = await axios.get("https://api.unsplash.com/search/photos",{
+      params:{
+        query:search
+      },
+      headers:{
+Authorization: "Client-ID ouowgsEn9JTAM7y-LFzVEsGe0z00g5pcTTDnWv_DuVY"
+      }
+    })
+    this.setState({
+      images:result.data.results
+    });
+
+  };
   render() {
     return (
       <div className="app-container">
-        <SearchBar onSearchImage={this.onSearchImage}/>
+        <SearchBar onSearchImage={this.onSearchImage} />
+        {this.state.images.length}
       </div>
     );
   }
